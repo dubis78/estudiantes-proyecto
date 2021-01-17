7f35c8f00664c0ae8f9a1f7d45c587f9de7438d8
dubis78 <dubis85201@hotmail.com>
2021-01-17 17:23:31 -0500

    fixing some services

--- routes/actores.js
+++ routes/actores.js
@@ -35,53 +35,52 @@ router.get("/actores", async(req,res) =>{
 
 
 router.post('/actor', async (req, res) => {
-  try {
-      const {
-          documento,
-          tipo_documento,
-          nombres,
-          apellidos,
-          contrasena,
-          correo,
-          telefono_celular,
-          numero_expediente,
-          genero,
-          fecha_nacimiento,
-          estado_actor_id,
-          institucion_id,
-          tipo_actor_id,
-          fecha_creacion,
-          fecha_actualizacion
-         } = req.body
-      const [rows, fields] = await cnn_mysql.execute(`INSERT INTO actores(documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion,fecha_actualizacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion, fecha_actualizacion])
-      //!without const promiseConnection_mysql=connection_mysql.promise(); IN database we have to use:
-      //const [rows, fields] = await cnn_mysql.promise().execute(`INSERT INTO actores(documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion,fecha_actualizacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion, fecha_actualizacion])
-      if (rows.affectedRows > 0) {
-          res.json({
-              id: rows.insertId,
-              documento: documento,
-              tipo_documento: tipo_documento,
-              nombres: nombres,
-              apellidos: apellidos,
-              contrasena: contrasena,
-              correo: correo,
-              telefono_celular: telefono_celular,
-              numero_expediente: numero_expediente,
-              genero: genero,
-              fecha_nacimiento: fecha_nacimiento,
-              estado_actor_id: estado_actor_id,
-              institucion_id: institucion_id,
-              tipo_actor_id: tipo_actor_id,
-              fecha_creacion: fecha_creacion,
-              fecha_actualizacion: fecha_actualizacion
-          })
-      } else {
-          res.json({})
-      }
-  } catch (e) {
-      res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
-  }
-})
+    try {
+        const {
+            documento,
+            tipo_documento,
+            nombres,
+            apellidos,
+            contrasena,
+            correo,
+            telefono_celular,
+            numero_expediente,
+            genero,
+            fecha_nacimiento,
+            estado_actor_id,
+            institucion_id,
+            tipo_actor_id,
+            fecha_creacion,
+            fecha_actualizacion
+        } = req.body
+        const [rows, fields] = await cnn_mysql.promise().execute(`INSERT INTO actores(documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion,fecha_actualizacion) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [documento, tipo_documento, nombres, apellidos, contrasena, correo, telefono_celular, numero_expediente, genero, fecha_nacimiento, estado_actor_id, institucion_id, tipo_actor_id, fecha_creacion, fecha_actualizacion])
+  
+        if (rows.affectedRows > 0) {
+            res.json({
+                id: rows.insertId,
+                documento: documento,
+                tipo_documento: tipo_documento,
+                nombres: nombres,
+                apellidos: apellidos,
+                contrasena: contrasena,
+                correo: correo,
+                telefono_celular: telefono_celular,
+                numero_expediente: numero_expediente,
+                genero: genero,
+                fecha_nacimiento: fecha_nacimiento,
+                estado_actor_id: estado_actor_id,
+                institucion_id: institucion_id,
+                tipo_actor_id: tipo_actor_id,
+                fecha_creacion: fecha_creacion,
+                fecha_actualizacion: fecha_actualizacion
+            })
+        } else {
+            res.json({})
+        }
+    } catch (e) {
+        res.status(500).json({errorCode : e.errno, message : "Error en el servidor"})
+    }
+  })
 
 router.patch('/actor/:id', async(req, res) => {
     try {
